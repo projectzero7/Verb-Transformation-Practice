95bd36b2c7327d3be81c61905784bef9192f4627
Josiah Egner <projectzero7@gmail.com>
2016-02-08 10:13:11 -0600

    Add Main class

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// Import needed files
+
+// Main class
+public class Main {
+	// Main application entry point
+	public static void main(String[] args) {
+		// Print out a starting message
+		System.out.println( "Hello, world!" );
+	}
+}
